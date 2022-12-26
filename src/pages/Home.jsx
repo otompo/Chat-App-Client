6638d92c7d7c039ffd3c901576b9695ed3c938b8
Abagna <abagnasalifusasco@gmail.com>
2022-12-26 14:12:29 +0000

    latest commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React,{useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from "styled-components"
 
 function Home(props) {
 
@@ -11,10 +12,32 @@ function Home(props) {
       },[]);
 
     return (
-        <div>
+        <Container>
             <h1>Home</h1>
-        </div>
+        </Container>
     );
 }
-
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  h1{
+    color:white
+  }
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 export default Home;
